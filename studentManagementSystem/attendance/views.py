a2fc48e3a77b901428a7ca826fdd23ac3a2d1d35
hiki7 <rsaliyev03@gmail.com>
2024-11-17 23:36:47 +0500

    add: attendance viewset

--- studentManagementSystem/attendance/views.py
+++ studentManagementSystem/attendance/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Attendance
+from .serializers import AttendanceSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class AttendanceViewSet(ModelViewSet):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+    permission_classes = [IsAuthenticated]

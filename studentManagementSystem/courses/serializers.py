a0c6d4f3bb33f4cd615b119a7e4c41085c2433a8
hiki7 <rsaliyev03@gmail.com>
2024-11-17 23:07:44 +0500

    add: serializer for enrollment model

--- studentManagementSystem/courses/serializers.py
+++ studentManagementSystem/courses/serializers.py
@@ -10,3 +10,12 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ["id", "name", "description", "instructor"]
+
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    student = StudentSerializer(read_only=True)
+    course = CourseSerializer(read_only=True)
+
+    class Meta:
+        model = Enrollment
+        fields = ["id", "student", "course", "enrollment_date"]

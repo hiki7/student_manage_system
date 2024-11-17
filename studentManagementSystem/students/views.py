a8cc7c3698232a5bdccc7bcc1df5ddff545d2309
hiki7 <rsaliyev03@gmail.com>
2024-11-17 22:37:37 +0500

    add: serializer for student model

--- studentManagementSystem/students/views.py
+++ studentManagementSystem/students/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class StudentViewSet(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes = [IsAuthenticated]

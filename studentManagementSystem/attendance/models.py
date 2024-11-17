3ec41b31fa529d429f6e1bf368c1a16411728652
hiki7 <rsaliyev03@gmail.com>
2024-11-17 23:33:25 +0500

    add: attendance model

--- studentManagementSystem/attendance/models.py
+++ studentManagementSystem/attendance/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Attendance(models.Model):
+    student = models.ForeignKey("students.Student", on_delete=models.CASCADE)
+    course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
+    date = models.DateField()
+    status = models.CharField(
+        max_length=10, choices=[("present", "Present"), ("absent", "Absent")]
+    )

8534a0cb59b26b588602acbfbb6ddf152ccbf9ef
hiki7 <rsaliyev03@gmail.com>
2024-11-17 23:31:19 +0500

    add: endpoints for grades app

---
+++ studentManagementSystem/grades/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+from .views import GradeViewSet
+
+router = DefaultRouter()
+
+router.register("", GradeViewSet)
+
+urlpatterns = router.urls
--- studentManagementSystem/studentManagementSystem/settings.py
+++ studentManagementSystem/studentManagementSystem/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "users",
     "students",
     "courses",
+    "grades",
 ]
 
 MIDDLEWARE = [

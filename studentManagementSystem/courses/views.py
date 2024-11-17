a43445632390bb0d60326710168845d0e31c30e5
hiki7 <rsaliyev03@gmail.com>
2024-11-17 23:11:48 +0500

    add: course viewset

--- studentManagementSystem/courses/views.py
+++ studentManagementSystem/courses/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Course, Enrollment
+from .serializers import CourseSerializer, EnrollmentSerializer
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ("name",)

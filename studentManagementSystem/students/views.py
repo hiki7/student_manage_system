74b70bf9b3dd546b336e0dbcfc2e87d6183bdbc6
hiki7 <rsaliyev03@gmail.com>
2024-11-17 22:48:18 +0500

    add: filters for student_viewset

--- studentManagementSystem/students/views.py
+++ studentManagementSystem/students/views.py
@@ -2,9 +2,12 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class StudentViewSet(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ["dob", "registration_date"]

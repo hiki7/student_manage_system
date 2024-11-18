d3e34974e4e7d58d9c466e09b0d3ea3d78a5ac9a
hiki7 <rsaliyev03@gmail.com>
2024-11-18 17:12:34 +0500

    add: logging for registration, login and logout

--- studentManagementSystem/users/views.py
+++ studentManagementSystem/users/views.py
@@ -4,8 +4,30 @@ from .permissions import IsStudent, IsAdmin
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
 import logging
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 logger = logging.getLogger("custom")
+
+
+@receiver(user_logged_in)
+def log_user_login(sender, request, user, **kwargs):
+    logger.info(f"User logged in: {user.username}")
+
+
+@receiver(user_logged_out)
+def log_user_logout(sender, request, user, **kwargs):
+    logger.info(f"User logged out: {user.username}")
+
+
+@receiver(post_save, sender=User)
+def log_user_registration(sender, instance, created, **kwargs):
+    if created:
+        logger.info(
+            f"New user registered: {instance.username} - Email: {instance.email}"
+        )
 
 
 class UserViewSet(ModelViewSet):

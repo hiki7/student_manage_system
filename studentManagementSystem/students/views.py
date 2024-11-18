27cdb12a852aa0c0ca13443c4963dd4791d1e657
hiki7 <rsaliyev03@gmail.com>
2024-11-18 16:10:38 +0500

    add: permissions for students

--- studentManagementSystem/students/views.py
+++ studentManagementSystem/students/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+
+from users.permissions import IsStudent, IsAdmin
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -11,3 +13,10 @@ class StudentViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filter_fields = ["dob", "registration_date"]
+
+    def get_permission(self):
+        if self.action in ["update", "partial_update"]:
+            return [IsAuthenticated(), IsStudent()]
+        elif self.action in ["destroy"]:
+            return [IsAuthenticated(), IsAdmin()]
+        return [IsAuthenticated()]

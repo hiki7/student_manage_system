5fd729b8ef4082d0528739fbb22fd0f37f1ca092
hiki7 <rsaliyev03@gmail.com>
2024-11-18 16:37:15 +0500

    add: caching for course_viewset

--- studentManagementSystem/courses/views.py
+++ studentManagementSystem/courses/views.py
@@ -4,6 +4,9 @@ from .serializers import CourseSerializer, EnrollmentSerializer
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from users.permissions import IsTeacher, IsAdmin
+from django.core.cache import cache
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class CourseViewSet(ModelViewSet):
@@ -16,6 +19,25 @@ class CourseViewSet(ModelViewSet):
         if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAuthenticated(), IsTeacher() or IsAdmin()]
         return [IsAuthenticated()]
+
+    @action(detail=False, methods=["get"])
+    def cached_list(self, request):
+        user = request.user
+        cache_key = f"courses_list_{user.id}"
+        courses = cache.get(cache_key)
+
+        if not courses:
+            if user.role == "teacher":
+                courses = Course.objects.filter(instructor=user)
+            elif user.role == "student":
+                courses = Course.objects.filter(enrollment__student__user=user)
+            else:
+                courses = Course.objects.all()
+
+            cache.set(cache_key, courses, timeout=3600)
+
+        serializer = CourseSerializer(courses, many=True)
+        return Response(serializer.data)
 
 
 class EnrollmentViewSet(ModelViewSet):

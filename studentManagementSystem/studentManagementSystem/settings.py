04249249bbff24e93f567fcfb63fb53c5d22a01d
hiki7 <rsaliyev03@gmail.com>
2024-11-18 17:37:26 +0500

    add: configured celery in settings.py

--- studentManagementSystem/studentManagementSystem/settings.py
+++ studentManagementSystem/studentManagementSystem/settings.py
@@ -211,3 +211,10 @@ LOGGING = {
         },
     },
 }
+
+
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+
+INSTALLED_APPS += ["django_celery_beat"]

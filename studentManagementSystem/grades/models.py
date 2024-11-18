ac049b807e2f2fde9fea5e1ff35bb5c13390d381
hiki7 <rsaliyev03@gmail.com>
2024-11-18 17:56:47 +0500

    change: updated the data type of grade attribute in grade model

--- studentManagementSystem/grades/models.py
+++ studentManagementSystem/grades/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Grade(models.Model):
     student = models.ForeignKey("students.Student", on_delete=models.CASCADE)
     course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
-    grade = models.CharField(max_length=2)
+    grade = models.DecimalField(max_digits=5, decimal_places=2)
     teacher = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)

655c44ff683ff755b2750d011a410c52527473e8
hiki7 <rsaliyev03@gmail.com>
2024-11-18 17:41:19 +0500

    add: send_grade_update_notification task

--- studentManagementSystem/notifications/tasks.py
+++ studentManagementSystem/notifications/tasks.py
@@ -2,9 +2,10 @@ from celery import shared_task
 from django.core.mail import send_mail
 from users.models import CustomUser
 
+
 @shared_task
 def send_attendance_reminder():
-    students = CustomUser.objects.filter(role='student')
+    students = CustomUser.objects.filter(role="student")
     for student in students:
         send_mail(
             subject="Daily Attendance Reminder",
@@ -13,3 +14,17 @@ def send_attendance_reminder():
             recipient_list=[student.email],
         )
     return f"Sent attendance reminders to {students.count()} students."
+
+
+@shared_task
+def send_grade_update_notification(student_email, course_name, grade):
+    """
+    Notify a student when their grade is updated.
+    """
+    send_mail(
+        subject="Grade Update Notification",
+        message=f"Your grade for the course {course_name} has been updated to {grade}.",
+        from_email="admin@school.com",
+        recipient_list=[student_email],
+    )
+    return f"Sent grade update notification to {student_email}."

f826732a3d12526aa186361d88450f0a81fef98f
hiki7 <rsaliyev03@gmail.com>
2024-11-18 18:39:15 +0500

    change: updated the course serializer so that the instructor will automatically be set to the authenticated user

--- studentManagementSystem/courses/serializers.py
+++ studentManagementSystem/courses/serializers.py
@@ -11,6 +11,12 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ["id", "name", "description", "instructor"]
 
+    def create(self, validated_data):
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            validated_data["instructor"] = request.user
+        return super().create(validated_data)
+
 
 class EnrollmentSerializer(serializers.ModelSerializer):
     student = StudentSerializer(read_only=True)
